655fce6eb75bf0f76dbe03ceb81f63af91f02cb3
AakibAnsari <aakiba6619@gmail.com>
2023-03-30 03:14:22 +0530

    add delete function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var itemlist = document.getElementById('items');
 var fname= document.getElementById('fname');
 var lname= document.getElementById('lname');
 var email= document.getElementById('email');
+
 form.addEventListener('submit', addItem);
+itemlist.addEventListener('click', removeItem);
 
 function addItem(e){
     e.preventDefault();
@@ -11,10 +13,20 @@ function addItem(e){
     var lname = document.getElementById('lname').value;
     var email = document.getElementById('email').value;
     var li = document.createElement('li');
-    li.className='list-group'
+    li.className='list-group-item'
     li.appendChild(document.createTextNode(fname));
     li.appendChild(document.createTextNode(lname));
     li.appendChild(document.createTextNode(email));
+
+    //create delete button element
+    var deletebtn = document.createElement('button');   
+    deletebtn.className = 'btn btn-danger btn-sm float-right delete';
+    deletebtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deletebtn);
+    itemlist.appendChild(li); 
+
+
+
     itemlist.appendChild(li);
 
     //storing data in storage
@@ -29,5 +41,16 @@ function addItem(e){
     console.log(myobjserializeed);   
 }
 
+//remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you Sure')){
+            var li = e.target.parentElement;
+            var email = document.getElementById('email').value;
+            itemlist.removeChild(li);
+            localStorage.removeItem(email);
+        }
+    }
 
+}
 
